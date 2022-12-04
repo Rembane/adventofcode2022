1ec31aa4e0b9553509078998285185ee18058ff8
Andreas Ekeroot <andeke@gmail.com>
2022-12-04 17:09:46 +0100

    Solution 3.2

--- 3/Main.hs
+++ 3/Main.hs
@@ -15,6 +15,16 @@ toPoints = fromJust . (`M.lookup` charMap)
 both :: (a -> b) -> (a, a) -> (b, b)
 both f (x1, x2) = (f x1, f x2)
 
+triplets :: [a] -> [[a]]
+triplets xs = case splitAt 3 xs of
+  ([], [])   -> []
+  (xs1, xs2) -> xs1:triplets xs2
+
 main :: IO ()
-main = print . sum . concatMap (map toPoints . S.toList . uncurry S.intersection . both (S.fromList . B.unpack) . uncurry B.splitAt . ((`div` 2) . B.length &&& id)) . filter (/= B.empty) . B.split '\n' =<< B.readFile "input"
+main = do
+  backpacks <- filter (/= B.empty) . B.split '\n' <$> B.readFile "input"
+  (putStrLn . ("Solution 1: " <>) . show . sum . concatMap (map toPoints . S.toList . uncurry S.intersection . both (S.fromList . B.unpack) . uncurry B.splitAt . ((`div` 2) . B.length &&& id))) backpacks
+
+  (putStrLn . ("Solution 2: " <>) . show . sum . concatMap (map toPoints . S.toList . foldl1 S.intersection . map (S.fromList . B.unpack)) . triplets) backpacks
+
 
