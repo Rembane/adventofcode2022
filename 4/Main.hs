b2f5cf077ec8c12bab13d31bf1cbcad8d141ebe3
Andreas Ekeroot <andeke@gmail.com>
2022-12-04 17:51:33 +0100

    Solve 4.1

---
+++ 4/Main.hs
@@ -0,0 +1,18 @@
+module Main where
+
+import Control.Arrow
+import Data.Char
+import Data.List
+
+parseLine :: String -> (Int, Int, Int, Int)
+parseLine s = let (i1, s1) = parser s
+                  (i2, s2) = parser s1
+                  (i3, s3) = parser s2
+                  (i4, s4) = parser s3
+               in (i1,i2,i3,i4)
+  where parser = (read *** drop 1) . span isDigit
+
+main :: IO ()
+main = do
+  pairs <- filter (\(i1,i2,i3,i4) -> i1 <= i3 && i2 >= i4 || i1 >= i3 && i2 <= i4) . map parseLine . lines <$> readFile "input"
+  print $ length pairs
