9730cce00040c1422587ac8235e89e3b07cd4365
Andreas Ekeroot <andeke@gmail.com>
2022-12-21 01:55:02 +0100

    Solution 6.2

--- 6/Main.hs
+++ 6/Main.hs
@@ -8,5 +8,13 @@ chunker :: String -> [String]
 chunker (c1:c2:c3:c4:s) = [c1,c2,c3,c4] : chunker (c2:c3:c4:s)
 chunker _               = []
 
+chunker2 :: String -> [String]
+chunker2 s = case splitAt 14 s of
+               (prefix, rest) | length prefix == 14 -> prefix : chunker2 (tail prefix ++ rest)
+                              | otherwise           -> []
+
 main :: IO ()
-main = print . head . catMaybes . zipWith (\i s -> if s == nub s then Just (i, s) else Nothing) [4,5..] . chunker . filter isLower =<< readFile "input"
+main = do
+  contents <- readFile "input"
+  putStrLn $ "Solution 1: " ++ (show . head . catMaybes . zipWith (\i s -> if s == nub s then Just (i, s) else Nothing) [4,5..] . chunker . filter isLower) contents
+  putStrLn $ "Solution 2: " ++ (show . head . catMaybes . zipWith (\i s -> if s == nub s then Just (i, s) else Nothing) [14,15..] . chunker2 . filter isLower) contents
